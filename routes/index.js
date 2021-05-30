74c02459f24428833d946442a7335d20fc609d4c
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2021-05-30 09:40:10 +0500

    gitignore file added

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const landing=require("../controllers/landing")
 /* GET home page. */
 router.get('/', landing.get_landing);
-
+router.post('/', landing.submit_lead);
 module.exports = router;
